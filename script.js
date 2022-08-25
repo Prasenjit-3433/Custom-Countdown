e919f4edf1f33705006ec9ee643ad62b735c5187
Prasenjit Sutradhar <prasenjitsutradhar3433@gmail.com>
2022-08-25 09:17:37 +0530

    form submission handling

--- script.js
+++ script.js
@@ -2,6 +2,21 @@ const inputContainer = document.getElementById('input-container');
 const countdownForm = document.getElementById('countdownForm');
 const dateElement = document.getElementById('date-picker');
 
+let countdownTitle = '';
+let countdownDate = '';
+
 // Set Date Input Min with Today's Date
 const today = new Date().toISOString().split('T')[0];
 dateElement.setAttribute('min', today);
+
+// Take values from form Input
+function updateCountdown(event) {
+    event.preventDefault();
+    countdownTitle = this[0].value;
+    countdownDate = this[1].value;
+    console.log(countdownTitle, countdownDate);
+}
+
+
+// Event Listeners
+countdownForm.addEventListener('submit', updateCountdown);
